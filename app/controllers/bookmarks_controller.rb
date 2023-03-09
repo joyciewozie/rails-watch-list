a44185f2fccc5a2660964e1f2e170e0c6cedf16f
Joyce <121034558+joyciewozie@users.noreply.github.com>
2023-03-09 15:11:07 +0800

    new and create actions in bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,17 @@
 class BookmarksController < ApplicationController
+  def new
+    # need @list in bookmarks form
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.create(bookmark_params)
+  end
+
+  private
+
+  def lists_article
+    params.require(:bookmark).permit(:comment)
+  end
 end
