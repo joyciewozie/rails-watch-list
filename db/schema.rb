0273800bf35b6a6c24d2b4e06a770109a51fd33a
Joyce <121034558+joyciewozie@users.noreply.github.com>
2023-03-09 14:10:58 +0800

    adds unique index for movie title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_055903) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_060948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_055903) do
     t.decimal "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_movies_on_title", unique: true
   end
 
   add_foreign_key "bookmarks", "lists"
